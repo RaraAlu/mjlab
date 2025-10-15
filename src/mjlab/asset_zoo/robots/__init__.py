00a442ac18050eeffc02000809f0349d0b73d3b5
云海 <zhangsanjh42@126.com>
2025-10-15 18:19:37 +0800

    ---

--- src/mjlab/asset_zoo/robots/__init__.py
+++ src/mjlab/asset_zoo/robots/__init__.py
@@ -1,7 +1,9 @@
 from mjlab.asset_zoo.robots.unitree_g1.g1_constants import G1_ROBOT_CFG
 from mjlab.asset_zoo.robots.unitree_go1.go1_constants import GO1_ROBOT_CFG
+from mjlab.asset_zoo.robots.agibot_x1.x1_constants import X1_ROBOT_CFG
 
 __all__ = (
+  "X1_ROBOT_CFG",
   "G1_ROBOT_CFG",
   "GO1_ROBOT_CFG",
 )
