15a1c8d280dc9b38e621a59cd97fa2491337ec98
云海 <zhangsanjh42@126.com>
2025-10-20 15:06:05 +0800

    ---

--- play.sh
+++ play.sh
@@ -3,3 +3,4 @@ uv run python -m mjlab.scripts.play_joystick Mjlab-Velocity-Flat-Agibot-X1-Play 
     --checkpoint-file ~/Work/mjlab/logs/rsl_rl/x1_velocity/2025-10-20_09-21-07/model_29999.pt \
     --num-envs 1 \
     --agent joystick \
+    # --viewer viser
